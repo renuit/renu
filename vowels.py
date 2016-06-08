6015d80c264b4c3e1731c8dfa3c5f276be00c4fe
renuit <renukadevi.it13@bitsathy.ac.in>
2016-06-08 11:27:52 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -2,5 +2,5 @@ m="hello people"
 print(m)
 r=m[::-1]
 print(r)
-s=r.replace('a','#').replace('e','#').replace('i','#').replace('o','#').replace('u','#')
+s=r.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
 print s
